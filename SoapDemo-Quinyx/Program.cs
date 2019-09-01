81a80d98c9f7dbd1d53e0c5f950e137c658f4713
MMorsell <Erazap@LIVE.SE>
2019-09-01 11:16:34 +0200

    Add project files.

---
+++ SoapDemo-Quinyx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoapDemo_Quinyx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
